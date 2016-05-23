fd6b25b04c5eff1f437b6798ce20bf11ba2c4d24
rogetsun <songyw163@163.com>
2016-05-23 19:32:55 +0800

    commit

--- test/TestRequestSender.java
+++ test/TestRequestSender.java
@@ -1,3 +1,4 @@
+import com.uv.utils.MD5;
 import com.uv.utils.http.HttpRequestSender;
 import net.sf.json.JSONObject;
 
@@ -8,8 +9,17 @@ import java.io.IOException;
  */
 public class TestRequestSender {
     public static void main(String[] args) throws IOException {
-        String s = HttpRequestSender.post("http://localhost:8000", JSONObject.fromObject("{name:'litx'}"));
+        //带cookie测试session,以登录为例子
+        JSONObject cookie = new JSONObject();
+        String s = HttpRequestSender.post("http://localhost:8080/credit/to_login", JSONObject.fromObject("{login_no:'lipeng', login_password:'" + MD5.string2md5("lipeng") + "'}"), cookie);
+        JSONObject ret = JSONObject.fromObject(s);
+        if (ret.getInt("ret_code") != 0) {
+            System.out.println(ret);
+            return;
+        }
+        s = HttpRequestSender.get("http://127.0.0.1:8080/credit/users", null, cookie);
         System.out.println(s);
+        System.out.println(cookie);
     }
 }
 
