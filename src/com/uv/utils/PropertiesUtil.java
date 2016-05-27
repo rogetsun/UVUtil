5ec32cf2a8839f4868e08b3888fffc9445433e46
rogetsun <songyw163@163.com>
2016-05-27 12:36:09 +0800

    commit

--- src/com/uv/utils/PropertiesUtil.java
+++ src/com/uv/utils/PropertiesUtil.java
@@ -12,12 +12,14 @@ public class PropertiesUtil {
     public static JSONObject loadProperties(String propertiesFile) throws IOException {
         Properties p = new Properties();
         try {
-            InputStream in = ClassLoader.getSystemResourceAsStream(propertiesFile);
+            //读取jar包外的和jar包同级的位置
+            InputStream in = new BufferedInputStream(new FileInputStream(propertiesFile));
             p.load(in);
         } catch (IOException e) {
             e.printStackTrace();
             try {
-                InputStream in = new BufferedInputStream(new FileInputStream(propertiesFile));
+                //读取jar包内的位置
+                InputStream in = ClassLoader.getSystemResourceAsStream(propertiesFile);
                 p.load(in);
             } catch (IOException e1) {
                 e1.printStackTrace();
